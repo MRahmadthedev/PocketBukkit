68d3fa37d29a4cdf75483838f732686e3fd2aa14
Philip Shilling <comedyman937@yahoo.com>
2014-05-18 10:57:10 -0400

    Create Color.java

---
+++ PocketBukkit/Java/Bukkit/Format/Color.java
@@ -0,0 +1,24 @@
+/*
+ *  ____            _        _   ___  _   _  _     _     _    _
+ * |  _ \ ___   ___| | _____| |_|   \| | | || | __| | __|_| _| |_
+ * | |_) / _ \ / __| |/ / _ \ __|  _/| | | || |/ /| |/ / _ |__ __|
+ * |  __/ (_) | (__|   <  __/ |_|   \| |_| ||   < |   < | |  | |
+ * |_|   \___/ \___|_|\_\___|\__|___/\_____/|_|\_\|_|\_\|_|  |_|
+ *
+ */
+
+package Bukkit.Format;
+
+    public class Color{
+
+        public static final String FORMAT_RESET = "\u001B[0m";
+        public static final String FORMAT_BLACK = "\u001B[30m";
+        public static final String FORMAT_RED = "\u001B[31m";
+        public static final String FORMAT_GREEN = "\u001B[32m";
+        public static final String FORMAT_YELLOW = "\u001B[33m";
+        public static final String FORMAT_BLUE = "\u001B[34m";
+        public static final String FORMAT_PURPLE = "\u001B[35m";
+        public static final String FORMAT_CYAN = "\u001B[36m";
+        public static final String FORMAT_WHITE = "\u001B[37m";
+
+    }
